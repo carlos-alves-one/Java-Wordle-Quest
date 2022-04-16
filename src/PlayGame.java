474bbd86b19b76dd83a44befc702d3a6486bc2d0
Carlos Alves <carlos.alves.one@gmail.com>
2022-04-16 17:06:44 +0100

    Created panels and used with border layout

--- src/PlayGame.java
+++ src/PlayGame.java
@@ -6,13 +6,16 @@
  */
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 /* declare and extend the class to include JFrame and Key Listener */
 public class PlayGame extends JFrame implements KeyListener {
 
-
+    /* declare frame as global */
+    JFrame frame;
 
     /* declare default constructor for this class */
     PlayGame() {
@@ -21,8 +24,32 @@ public class PlayGame extends JFrame implements KeyListener {
         String namePlayer = JOptionPane.showInputDialog("Please enter your name: ");
         System.out.println("The player name is: " + namePlayer);
 
+        /* create border for frame */
+        Border border = BorderFactory.createLineBorder(Color.GREEN,3);
+
+        /* create panels */
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
+
+        /* set background color of the panels */
+        panel1.setBackground(Color.black);
+        panel2.setBackground(Color.black);
+        panel3.setBackground(Color.black);
+        panel4.setBackground(Color.magenta);
+        panel5.setBackground(Color.blue);
+
+        /* set size of the panels */
+        panel1.setPreferredSize(new Dimension(100,50));
+        panel2.setPreferredSize(new Dimension(20,50));
+        panel3.setPreferredSize(new Dimension(20,50));
+        panel4.setPreferredSize(new Dimension(100,200));
+        panel5.setPreferredSize(new Dimension(100,50));
+
         /* create and setup frame */
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setTitle("## WORDLE GAME - WELCOME ##");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false); // prevents frame from being resized
@@ -32,6 +59,13 @@ public class PlayGame extends JFrame implements KeyListener {
 
         /* set up position of the frame at the center of the screen */
         frame.setLocationRelativeTo(null);
+
+        /* add panels to the frame */
+        frame.add(panel1, BorderLayout.NORTH);
+        frame.add(panel2, BorderLayout.WEST);
+        frame.add(panel3, BorderLayout.EAST);
+        frame.add(panel4, BorderLayout.SOUTH);
+        frame.add(panel5, BorderLayout.CENTER);
     }
 
     @Override

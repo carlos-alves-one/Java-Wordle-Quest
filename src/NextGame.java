801a5f340e987ba47d566ab368c07aeee8980e13
Carlos Alves <carlos.alves.one@gmail.com>
2022-04-23 17:48:58 +0100

    End game button added action listener - terminates app

--- src/NextGame.java
+++ src/NextGame.java
@@ -9,16 +9,20 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Objects;
 
 /* declare and extend the class to include JFrame and Key Listener */
 public class NextGame extends JFrame implements ActionListener{
+
+    /* declare buttons global */
+    JButton newGame;
+    JButton topGame;
+    JButton endGame;
 
     /* declare default constructor for this class */
     NextGame() {
 
         /* create and setup frame */
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         this.setSize(500,100);
         this.setLayout(new GridLayout(1,3,10,10));
         this.setTitle("Please press one button");
@@ -28,21 +32,24 @@ public class NextGame extends JFrame implements ActionListener{
         this.setLocationRelativeTo(null);
 
         /* create radio buttons and add to the frame */
-        JButton newGame = new JButton("Start a new game");
+        newGame = new JButton("Start a new game");
         newGame.setFont(new Font("Clear Sans", Font.BOLD, 14));
         newGame.setFocusable(false);
         newGame.setBackground(new Color(245,149,66));
+        newGame.addActionListener(this);
 
-        JButton topGame = new JButton("Top five players");
+        topGame = new JButton("Top five players");
         topGame.setFont(new Font("Clear Sans", Font.BOLD, 14));
         topGame.setFocusable(false);
         topGame.setBackground(new Color(66,167,245));
+        topGame.addActionListener(this);
 
-        JButton endGame = new JButton("End of game");
+        endGame = new JButton("End of game");
         endGame.setFont(new Font("Clear Sans", Font.BOLD, 14));
         endGame.setFocusable(false);
         endGame.setBackground(new Color(149,245,66));
-        
+        endGame.addActionListener(this);
+
         this.add(newGame);
         this.add(topGame);
         this.add(endGame);
@@ -55,5 +62,9 @@ public class NextGame extends JFrame implements ActionListener{
     /* method invoked when one of the radio buttons are selected */
     public void actionPerformed(ActionEvent e) {
 
+        /* check we press the end game button */
+        if (e.getSource() == endGame) {
+            System.exit(0);
+        }
     }
 }

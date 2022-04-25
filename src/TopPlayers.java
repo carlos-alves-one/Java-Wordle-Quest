28fc72adc29608a934c27e41ea8afbc901985cde
Carlos Alves <carlos.alves.one@gmail.com>
2022-04-25 14:16:27 +0100

    Class Top Players - declare libraries

--- src/TopPlayers.java
+++ src/TopPlayers.java
@@ -2,8 +2,18 @@
  * Goldsmiths University of London
  * Author...: Carlos Manuel de Oliveira Alves
  * Student..: cdeol003
- * Created..: 23/04/2022
+ * Created..: 25/04/2022
  */
+
+/* declare all libraries to be used with this class */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Objects;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /* declare class to display top players */
 public class TopPlayers {

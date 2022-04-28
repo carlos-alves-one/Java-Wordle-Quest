ddfb4eaa00c0874b903653bed91b1d203578acf4
Carlos Alves <carlos.alves.one@gmail.com>
2022-04-28 16:17:05 +0100

    Create new class player to work with array list dataDatabase

---
+++ src/game/Player.java
@@ -0,0 +1,16 @@
+
+/*
+ * Goldsmiths University of London
+ * Author...: Carlos Manuel de Oliveira Alves
+ * Student..: cdeol003
+ * Created..: 28/04/2022
+ */
+
+/* declare package of this class */
+package game;
+
+/* declare class for player */
+public class Player {
+
+    
+}

412c3120d12a2382adfbabea5bc669057db1e0b8
Carlos Alves <carlos.alves.one@gmail.com>
2022-04-28 11:26:46 +0100

    Create new class database players

--- src/game/PlayGame.java
+++ src/game/PlayGame.java
@@ -8,7 +8,6 @@
 
 /* declare package and all libraries to be used with this class */
 package game;
-import game.MenuGame;
 import main.MainApp;
 
 import javax.swing.*;
---
+++ src/game/databasePlayers.java
@@ -0,0 +1,19 @@
+
+/*
+ * Goldsmiths University of London
+ * Author...: Carlos Manuel de Oliveira Alves
+ * Student..: cdeol003
+ * Created..: 28/04/2022
+ */
+
+/* declare package and all libraries to be used with this class */
+package game;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+/* declare and extend the class to include JFrame and Key Listener */
+public class databasePlayers extends JFrame implements ActionListener {
+
+    
+}
